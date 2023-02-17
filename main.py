9b559bc9479ea5bdc4ce74ca11f256b95662fd1e
mlc-delgado <mr.mlcdelgado@gmail.com>
2023-02-16 19:40:29 -0800

    Added logging

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import customtkinter
 from data import forecast, active_alerts
 from config import update
 import yaml
+import logging
 
+# set up logger
+logger = logging.getLogger(__name__)
+# set the logging level
+logging.basicConfig(level=logging.INFO)
+# set the logging format
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# set the logging file
+logging.basicConfig(filename='nws_weather_ctk.log')
+
+# set the frame refresh rate in milliseconds
 refresh_ms = 300000
 
 # load the config file
@@ -12,7 +23,7 @@ def load_config():
     f.close()
     return config
 
-# Set the appearance mode to dark
+# Set the appearance mode to system theme
 customtkinter.set_appearance_mode('system')
 
 # define InputFrame and WeatherFrame classes
@@ -38,7 +49,7 @@ class WeatherFrame(customtkinter.CTkFrame):
         # call the parent class constructor
         super().__init__(*args, **kwargs)
 
-        # add buttons to get weather and update location
+        # add button to set location
         self.button = customtkinter.CTkButton(master=self, text='Set Location', command=self.display_weather)
         self.button.pack(pady=12, padx=12)
 
@@ -47,31 +58,35 @@ class WeatherFrame(customtkinter.CTkFrame):
         try:
             self.forecastLabel.pack_forget()
             self.alertLabel.pack_forget()
-        except:
+        # ignore exceptions in case labels don't exist
+        except Exception:
             pass
 
         # load the config file
         config = load_config()
 
-        # if the config file is empty or doesn't contain all the required keys, update the location
-        if not config or not all(key in config for key in ['city', 'county', 'gridX', 'gridY', 'latitude', 'longitude', 'office', 'postalCode', 'state']):
-            app.update_location()
-            config = load_config()
-
-        # check that the city and postal code match the config file, and if not update the location
-        if city != config['city'] or postal_code != config['postalCode']:
+        # if the config file is empty, or doesn't match the input city and postal code, or doesn't contain all the required keys, update the location config
+        if not config or city != config['city'] or postal_code != config['postalCode'] or not all(key in config for key in ['city', 'county', 'gridX', 'gridY', 'latitude', 'longitude', 'office', 'postalCode', 'state']):
             app.update_location()
             config = load_config()
     
         # fetch forecast and alert data
-        forecast_data = forecast(config)
-        alerts_data = active_alerts(config)
+        try:
+            forecast_data = forecast(config)
+            alerts_data = active_alerts(config)
+        # log an error if the data cannot be fetched
+        except Exception as e:
+            logger.error(f'Error fetching forecast data: {e}')
 
         # add label to show the current temperature and forecast
-        self.forecastLabel = customtkinter.CTkLabel(master=self, text='Weather for {city}, {state}\nTemperature: {temperature}°F\nForecast: {forecast}'.format(city=config['city'], state=config['state'], temperature=forecast_data['properties']['periods'][0]['temperature'], forecast=forecast_data['properties']['periods'][0]['shortForecast']))
-        self.forecastLabel.pack(pady=12, padx=12)
+        try:
+            self.forecastLabel = customtkinter.CTkLabel(master=self, text='Weather for {city}, {state}\nTemperature: {temperature}°F\nForecast: {forecast}'.format(city=config['city'], state=config['state'], temperature=forecast_data['properties']['periods'][0]['temperature'], forecast=forecast_data['properties']['periods'][0]['shortForecast']))
+            self.forecastLabel.pack(pady=12, padx=12)
+        # log an error if the forecast data cannot be parsed
+        except Exception as e:
+            logger.error(f'Error reading forecast data: {e}')
 
-        # update the label every minute
+        # update the label periodically
         self.forecastLabel.after(refresh_ms, self.display_weather)
 
         # add label to show active alerts if the alert contains the county name
@@ -81,7 +96,7 @@ class WeatherFrame(customtkinter.CTkFrame):
                     self.alertLabel = customtkinter.CTkLabel(master=self, text='Active alerts: {alert}'.format(alert=alert['properties']['event']))
                     self.alertLabel.pack(pady=12, padx=12)
                     
-                    # update the label every minute
+                    # update the label periodically
                     self.alertLabel.after(refresh_ms, self.display_weather)
     
     # update the location and display the weather
@@ -110,7 +125,11 @@ class App(customtkinter.CTk):
 
     # update the location
     def update_location(self):
-        update(self.input_frame.get_values()[0], self.input_frame.get_values()[1]) 
+        try:
+            update(self.input_frame.get_values()[0], self.input_frame.get_values()[1])
+        # log an error if the location cannot be updated
+        except Exception as e:
+            logger.error(f'Error updating location: {e}')
 
 # run the app
 if __name__ == '__main__':
